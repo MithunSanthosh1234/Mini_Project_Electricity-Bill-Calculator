7ec90dceeb93a8e157140ab419ed165e44206e2c
Mithun Santhosh <42834863+MithunSanthosh1234@users.noreply.github.com>
2019-01-23 11:22:19 +0530

    Update electicity bill.py

--- electicity bill.py
+++ electicity bill.py
@@ -1,52 +1,49 @@
-from tkinter import *
-from math import *
-def choice(units):
-    res.configure(text = "calculator: " + str(eval(entry.get())))
-    
 def domestic(units):
      if(units<=100):
         pay=0
         print("no need to pay")
      elif (units>100 and units<201):
-        unit=unit-100
+        units=units-100
         pay=units*2.50
         print("You have to pay:Rs.",pay,)
      elif(units>201 and units<501):
-         unit=unit-100
+         units=units-100
          pay=units*4
          print("You have to pay:Rs.",pay)
      else:
         pay=units*6
         print("You have to pay:Rs.",pay)
     
-
 def commercial(units):
     if(units<=100):
         print("no need to pay")
     elif (units>101 and units<=201):
-        unit=unit-100
+        units=units-100
         pay=units*4.50
         print("You have to pay:Rs.",pay)
     elif(units>201 and units<=500):
-        unit=unit-100
+        units=units-100
         pay=units*6
         print("You have to pay:Rs.",pay)
     else:
-        unit=unit-100
+        units=units-100
         pay=units*7
         print("You have to pay:Rs.",pay)
         
         
 def industry(units):
-    if(units<=1000):
+    if(units<=100):
         print("no need to pay")
-    elif(units>1000 and units<=5000):
-        unit=unit-100
-        pay=units*1
+    elif(units>101 and units<=200):
+        units=units-100
+        pay=units*6.50
         print("You have to pay:",pay)
+    elif(units>201 ana units<=500):
+        units=units-100
+        pay=units*8
     else:
-        unit=unit-100
-        pay=units*(2*1)
+        units=units-100
+        pay=units*8.50
         print("You have to pay:",pay)
 
 print ("Select your Locality")
